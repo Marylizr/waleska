20f0cd445ff9e2bc6e147b20b98ca4885e0dab8c
MaryLizR <marylizr@gmail.com>
2025-10-15 20:22:55 +0200

    !

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,6 @@ const LINKS = [
   { href: "/", label: "Home" },
   { href: "/#about", label: "About" },
   { href: "/#services", label: "Services" },
-  { href: "/#portfolio", label: "Portfolio" },
   { href: "/#contact", label: "Contact" },
 ];
 
